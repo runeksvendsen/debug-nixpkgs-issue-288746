4d4033013d29b0447185729ccc383cb3d33369b9
Rune K. Svendsen <runesvend@gmail.com>
2024-03-22 12:08:00 +0100

    Only build 'darling'

---
+++ build-darling.sh
@@ -0,0 +1,11 @@
+set -e
+
+NIXPKGS="$1"
+
+for drv in $(nix-build --option substituters "" --option store "local?root=$(pwd)/tmp_store" -A darwin.Libsystem "$NIXPKGS" 2>&1 |grep 'source.drv$'); do
+  if ! nix derivation show "$drv^*"|jq ".\"$drv\".env.urls"|grep darling; then
+    continue
+  fi
+
+  nix-build --check "$drv"
+done
