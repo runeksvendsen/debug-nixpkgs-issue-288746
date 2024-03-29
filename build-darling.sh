e580a4af270e3f1c8bd0fa6d8bfcaada2145098a
Rune K. Svendsen <runesvend@gmail.com>
2024-03-29 11:59:02 +0100

    Simplify build-darling.sh

--- build-darling.sh
+++ build-darling.sh
@@ -7,10 +7,8 @@ PKGS="$1"
 DRV=$(nix-instantiate --option substituters "" -A darwin.Libsystem "$PKGS")
 
 for source in $(nix derivation show "$DRV^*" |jq -r ".\"$DRV\".inputDrvs | keys[]"|grep "source.drv$"); do
-  if ! nix derivation show "$source^*"|jq ".\"$source\".env.urls"|grep darling; then
-    continue
+  if nix derivation show "$source^*"|jq ".\"$source\".env.urls"|grep darling; then
+    nix-build --check "$source"
   fi
-
-  nix-build --check $source
 done
 
